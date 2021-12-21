4e79bbbe24d1843b3bb9bcb522c55702b951bce9
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-12-21 11:46:31 -0500

    Add timmer
    -Add timer with redux-toolkit

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,23 +1,36 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from '../../store/slices/products';
+import { updateTime } from '../../store/slices/timer';
 import ProductCard from '../ProductCard';
 
 import './style.scss';
 
 const Gallery = () => {
   const { productList } = useSelector((state) => state.products);
+  const { dateNow } = useSelector((state) => state.timer);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
-  const title = 'Nombre de la galería';
+  useEffect(() => {
+    const countDownTimer = setTimeout(() => {
+      dispatch(updateTime());
+    }, 1000);
+    return () => clearTimeout(countDownTimer);
+  }, [dispatch, dateNow]);
   return (
     <div className="gallery">
-      <h1 className="gallery__title">{title}</h1>
+      <h1 className="gallery__title">Products</h1>
       <div className="gallery__grid">
         {productList.map((e) => (
-          <ProductCard key={e.id} title={e.title} img={e.image} />
+          <ProductCard
+            key={e.id}
+            title={e.title}
+            img={e.image}
+            id={e.id}
+            dateNow={dateNow}
+          />
         ))}
       </div>
     </div>
--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import Timer from '../Timer';
+
 import './styles.scss';
 
-const ProductCard = ({ title, img }) => {
+const ProductCard = ({ title, img, id, dateNow }) => {
+  const { timeLimit } = useSelector((state) => state.timer);
   return (
     <div className="productCard">
       <img className="productCard__img" src={img} alt={title} />
       <p className="productCard__title">{title} </p>
       <div className="productCard__footer">
-        <span className="productCard__counter">Aquí va el contador</span>
-        <button type="button" className="productCard__button">
-          Go to detail
-        </button>
+        <Timer id={id} className="productCard__counter" />
+        {timeLimit.length &&
+        timeLimit.find((e) => e.id === id).limit - dateNow >= 0 ? (
+          <button
+            type="button"
+            className="productCard__button"
+            onClick={() => console.log('working')}
+          >
+            Go to detail
+          </button>
+        ) : (
+          <button type="button" className="productCard__button">
+            Go to detail
+          </button>
+        )}
       </div>
     </div>
   );
@@ -19,6 +33,8 @@ const ProductCard = ({ title, img }) => {
 ProductCard.propTypes = {
   title: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  dateNow: PropTypes.number.isRequired,
 };
 
 export default ProductCard;
